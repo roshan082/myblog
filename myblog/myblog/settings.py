8307f5fec777f0b79e0b8598025265e9c775b3a7
maggot082 <sroshan20@tbc.edu.np>
2022-04-18 13:26:41 +0545

    Templates folder added to the app and configuration is done in settings.py file.

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'firstapp.templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
