57739f3f9f1f5af7d209e891e6553781d9317126
maggot082 <sroshan20@tbc.edu.np>
2022-04-20 17:02:14 +0545

    MYSQL Database connected to django, Also created a model.

--- myblog/firstapp/models.py
+++ myblog/firstapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class AppUser(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    middle_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(null=False, unique=True)
+    contact = models.CharField(null=False, unique=True, max_length=10)
+
+    class Meta:
+        db_table = "app_users"
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.mysql', # mysql driver link
+        'NAME' : 'myblogapp', # database name
+        'HOST' : 'localhost', # host --> localhost or 127.0.0.1
+        'USER' : 'root', # database user
+        'PASSWORD' : '', # database user password
+        'PORT' : '3306', # mysql port number
     }
+
 }
 
 
