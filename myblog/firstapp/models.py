03a698b5b61dc81a06272ac9382390d308c6e308
maggot082 <sroshan20@tbc.edu.np>
2022-04-28 22:04:38 +0545

    Models created for blog app.

--- myblog/firstapp/models.py
+++ myblog/firstapp/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -11,3 +12,45 @@ class AppUser(models.Model):
 
     class Meta:
         db_table = "app_users"
+
+class BlogAppUser(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    contact = models.CharField(max_length=15,null=True)
+    password = models.CharField(max_length=20)
+    profile_image = models.FileField(upload_to='images/profile_images/', null=True)
+    verification_code = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    is_removed = models.BooleanField(default=False)
+    created_at =models.DateTimeField(default=datetime.now(), blank=False)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    removed_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "app_blogusers"
+
+class BlogCategory(models.Model):
+    category = models.CharField(max_length=100)
+    parent_id = models.BigIntegerField(null=True)
+
+    class Meta:
+        db_table = "app_blogcategories"
+
+class BlogPost(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_description = models.TextField(max_length=500)
+    slug = models.CharField(max_length=200)
+    caregory_id = models.BigIntegerField(null=True)
+    post_image = models.FileField(upload_to='images/blog_posts/')
+    user_id = models.BigIntegerField(null=True)
+    post_status = models.CharField(null=True, max_length=20)
+    created_at =models.DateTimeField(default=datetime.now(), blank=False)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    removed_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "app_blogposts"
+
